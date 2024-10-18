acab376b69184fbc786833750b700c75d06d4981
Anusha Din <Anusha-N-Din@hotmail.com>
2024-10-18 21:19:16 +0100

    Started to implement a method to read text. Only placed out the skelleton of the class and a testing file which will use some form of py testing library. This will eventaully show the text onto the game screen (commandline).

---
+++ Reader.py
@@ -0,0 +1,13 @@
+
+class Reader():
+    def __init__(self):
+        self.file = None
+
+    def get_file(self):
+        return self.file
+    
+    def load_file(fileName):
+        pass
+
+    def readLineByLine(fileName):
+        pass
