ef31b6fbf1001a213e2e74263ecbb22e59d31b6c
Anusha Din <Anusha-N-Din@hotmail.com>
2024-10-21 16:56:46 +0100

    Written all the tests for the reader class with the placeholder methods.

--- testing or planning/Reader.py
+++ testing or planning/Reader.py
@@ -1,13 +1,30 @@
 
 class Reader():
+    """
+    Class used to display the text from files to 
+    the screen, which will be used to further the 
+    plot of the game.
+    :attribute file: the file that the reader is currently reading from 
+    """
     def __init__(self):
         self.file = None
 
     def get_file(self):
+        """
+        fetches the file currently being read.
+        :returns file: 
+        """
         return self.file
     
-    def load_file(fileName):
+    def __load_file(fileName):
+        """
+        used to load a new file into the class.
+        :param filename: string path of the file
+        """
         pass
 
-    def readLineByLine(fileName):
+    def read():
+        """
+        Method which actually displays the text onto screen.
+        """
         pass
--- testing or planning/ReaderTEST.py
+++ testing or planning/ReaderTEST.py
@@ -1,12 +1,27 @@
 import unittest
 import Reader
 
-class ReaderTEST(unittest.TestCase):
-    def __init__(self):
-        super.__init__()
-        self.reader = Reader()
 
-    def isFileNone(self, filename):
-        self.reader.load_file(filename)
+class ReaderTEST(unittest.TestCase):
+    """
+    Testing class for the Reader
+    """
+
+    def test_isFileNone(self):
+        reader = Reader()
+        reader.load_file("chapIintro")
         file = self.reader.get_file()
+        self.assertIsNotNone(file)
+
+    def test_isTheTextCorrect(self):
+        reader = Reader()
+        reader.load_file("chapIintro")
+        file = self.reader.get_file()
+        self.assertEquals(file[0],"==================CHAPTER I - HOME?======================")
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
 
