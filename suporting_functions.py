d3ff896c735981ea7aa555e110feca88017a86d7
Anusha Din <Anusha-N-Din@hotmail.com>
2024-12-05 18:52:37 +0000

    Changed it so that the input is represented in a list, letting string parsing function a bit easier.

--- suporting_functions.py
+++ suporting_functions.py
@@ -21,21 +21,23 @@ def string_parsing1(reader):
     while not (locked and fridge and comp):
         time.sleep(2)
         inp = input(">")
+        inp = inp.split()
 
         #first the general ones
-        if inp == "quit":
+        if "quit" in inp:
             sys.exit()
 
         #string parsing begins
+        
 
 
         #a sneeky test
-        if inp == "test":
+        if "test" in inp:
             display_text(reader, "chapterI", "test")
 
         #input not recognised
         else:
-            print("You can't do that yet")
+            print("\nYou can't do that yet")
 
 
 
