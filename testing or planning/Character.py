b46d0232567d5bea3104d4f261c75b0a329ead56
Anusha Din <Anusha-N-Din@hotmail.com>
2025-03-21 19:50:08 +0000

    completed testing for the character class. now I just need to integrate it into the game, and start to implement items so that an instance of character can actually be used. I also added minor changes to some of the text so that it shows better on the screen.

--- testing or planning/Character.py
+++ testing or planning/Character.py
@@ -3,26 +3,54 @@ class Character():
     Class used to track what a player has done and where
     they are in the game
     """
-    #singleton design pattern????
+    #singleton design pattern???? - maybe not bc it ~might~ be the basis of the devilmalz
 
     def __init__(self):
-        self.items = ["", "", "", ""] #fixed amount? I'm not sure becaus eof the suitcase
+        self.items = ["", "", "", ""] #fixed amount? I'm not sure because of the suitcase
+        self.itemNo = 0 
         self.currentRoom = None
         self.level = 1
 
     def get_items(self): return self.items
     def get_currentRoom(self): return self.currentRoom
     def get_level(self): return self.level
+    def get_itemNo(self): return self.itemNo
+
+    def itemNo_inc(self): self.itemNo = self.get_itemNo() + 1
+    def itemNo_dec(self): self.itemNo = self.get_itemNo() - 1
+    #using the set_itemNo(self.get_itemNo() + 1) wasn't working so I thougth I shoudl try this
+    #that probably wasn't causing the error but I prefer this method anyway; more abstraction but idk
+    #if it is more efficient
 
     def add_item(self, item):
         pass
 
     def show_inv(self): 
-        pass
+        if self.get_itemNo() == 0:
+            return "You currently don't have any items"
+        else:
+            itemsStr = "\n".join([str(item) for item in self.get_items()])
+            return "The items currently in your inventory are: \n%s" %(itemsStr)
 
-    def get_item():
-        pass
+    def add_item(self, item):
+        if not self.__isFull():
+            #add item
+            self.items[self.get_itemNo()] = item
+            self.itemNo_inc()
+            return "Item added to your inventory!"
+        else:
+            return "You have no more space in your inventory"
 
-    def __string__():
-        return ""
+
+    def __isFull(self):
+        return self.get_itemNo() == 4
+    
+
+    def __str__(self):
+        return "You are currently level %d" %(self.get_level())
+    #IF chapt1 THEN
+    #   just level
+    #ELSE
+    #   add in attack, defense and luck stats as well
+    #END IF
     
--- testing or planning/CharacterTEST.py
+++ testing or planning/CharacterTEST.py
@@ -9,26 +9,44 @@ class CharacterTEST(unittest.TestCase):
     def test_stats(self):
         player = Character()
         stats = str(player)
-        self.assertNotEqual("", stats)
+        self.assertEqual("You are currently level 1", stats)
+        print("\n stats test:")
+        print(stats)
 
     def test_showInvEmpty(self):
         player = Character()
         items_message = player.show_inv()
+        print("\n show empty test ")
+        print(items_message)
         self.assertEqual(items_message, "You currently don't have any items")
 
     def test_showInvWithItems(self):
         player = Character()
+        player.add_item("item")
+        player.add_item("another item")
         items_message = player.show_inv()
+        print("\n show some items test")
+        print(items_message)
         self.assertIsNotNone(items_message)
+        
+    
+    def test_addItem(self):
+        player = Character()
+        print(player.add_item("item"))
+        print("\n add items check")
+        player.show_inv()
+        self.assertEqual(1, player.get_itemNo())
 
     def test_itemsFull(self):
         player = Character()
-        for x in range(3):
+        print("\n inv ful test")
+        for x in range(4):
             player.add_item("item")
             #change to use item object after
         
         message = player.add_item("item")
         self.assertIsNotNone(message)
+        print(message)
 
 if __name__ == "__main__":
     unittest.main()
