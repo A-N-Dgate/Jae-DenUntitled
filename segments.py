a4c95f24547c73e7178e32f5c94bb1d82a8339fc
Anusha Din <Anusha-N-Din@hotmail.com>
2025-06-09 14:37:36 +0100

    I forgot to delete the alpha introduction befroe merging, so I have done that now.

--- segments.py
+++ segments.py
@@ -1,10 +1,7 @@
 from suporting_functions import *
 
 def Introduction():
-    welcome = open("descriptions/Introduction/welcome.txt")
-    text = combineText(welcome)
-    print(text)
-    next = input("press enter when you're done adjusting settings \n>")
+    pass
 
 def Chapter1(reader, player):
     display_text(reader, "ChapterI", None, False, True)
