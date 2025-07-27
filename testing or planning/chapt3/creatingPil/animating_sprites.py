c956afb767f98ead2a215ce8635e8b8d48ae475d
Anusha Din <Anusha-N-Din@hotmail.com>
2025-07-27 15:43:18 +0100

    created a master class for the devilmalz and started with the Pil class.

---
+++ testing or planning/chapt3/creatingPil/animating_sprites.py
@@ -0,0 +1,55 @@
+#adjusted from More Python Programming for the Absolute Beginner book
+import pygame
+from pygame.locals import *
+
+class my_sprite(pygame.sprite.Sprite):
+    def __init__(self, target):
+        pygame.sprite.Sprite.__init__(self)
+        self.master_image = None
+        self.image = None
+        self.frame = 0
+        self.old_frame = -1
+        self.frame_width = 1
+        self.frame_height = 1
+        self.first_frame = 0
+        self.columns = 1
+        self.last_frame = 0
+
+    #I don't think the devilmalz need to move, but I'll keep these here for now
+    def get_x(self): return self.rect.x
+    def set_x(self, value): self.rect.x = value
+
+    def get_y(self): return self.rect.y
+    def set_y(self, value): self.rect.y = value
+
+    def load(self, filename, width, height, columns):
+        #so this will be in the init of the devilmalz
+        self.master_image = pygame.image.load(filename).convert_alpha()
+        self.rect = Rect(0, 0, width, height) #magic numbers?
+        self.image = self.master_image.subsurface(self.rect)
+        self.frame_width = width
+        self.frame_height = height
+        self.columns = columns
+
+    def update(self, current_time, rate): 
+        #again, not sure if I need the x and y bc the devilmalz will stay in place
+        #--removed x and y from method
+        if current_time > self.last_time + rate:
+            self.frame += 1
+            if self.frame > self.last_frame:
+                self.frame = self.first_frame
+            self.last_time = current_time 
+
+        #build on current frame, only if it has been changed
+        if self.frame != self.old_frame:
+            #gets current frame by covering up the master image
+            frame_x = (self.frame % self.columns) * self.frame_width
+            frame_y = (self.frame // self.columns) * self.frame_height
+            self.rect = Rect(frame_x, frame_y, self.frame_width, self.frame_height)
+            self.image = self.master_image.subsurface(self.rect)
+            self.old_frame = self.frame #so then if the frame changes, they are different
+
+        #there should be a set x and set y here but its not needed
+
+
+
---
+++ testing or planning/chapt3/creatingPil/pil.py
@@ -0,0 +1,7 @@
+import pygame
+from pygame.locals import *
+from animating_sprites import *
+
+class Pil(my_sprite):
+    def __init__(self):
+        pass
