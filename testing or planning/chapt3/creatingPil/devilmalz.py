792faabdc8600197ae137253706e0057b1e182c3
Anusha Din <Anusha-N-Din@hotmail.com>
2025-07-27 15:50:06 +0100

    realised I needed to create a subclass of the sprite class I made, THEN the devilmalz individual classes.

---
+++ testing or planning/chapt3/creatingPil/devilmalz.py
@@ -0,0 +1,14 @@
+import pygame
+from pygame.locals import *
+from animating_sprites import *
+
+class Devilmalz(my_sprite):
+    #all the devilmalz should have the same animationm cycle
+    def __init__(self):
+        pass
+
+
+class Pil(Devilmalz):
+    #loads their own spritesheet + anything else
+    def __init__(self):
+        pass
--- testing or planning/chapt3/creatingPil/pil.py
+++
@@ -1,7 +0,0 @@
-import pygame
-from pygame.locals import *
-from animating_sprites import *
-
-class Pil(my_sprite):
-    def __init__(self):
-        pass
