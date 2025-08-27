e2b3a1025146c6d2448961591ab517932b481bdd
Anusha Din <Anusha-N-Din@hotmail.com>
2025-08-27 18:34:59 +0100

    Can now display text within the fight box, in the tests.

---
+++ testing or planning/chapt3/textScreen/Writer.py
@@ -0,0 +1,49 @@
+from Reader import *
+import pygame
+
+class Writer():
+    """
+    Class used to wrtie text to the 2d graphical screen.
+    :attribute screen: pygame display obeject; where the text will be displayed.
+    :attribute rect: pygame Rect object showing the bounds of where the texct can go.
+    """
+    def __init__(self, screen):
+        self.screen = screen
+        self.text = ""
+        self.rect = pygame.Rect(430,400,540,330) #trial and error testedchange for main code
+
+    def load_text(self, filepath):
+        """
+        Mathiod which takes text from a file and stroes it within the class.
+        :attribute filepath: the path of the file where the text is stored.
+        """
+        file = open(filepath)
+        self.text = self.get_text(filepath)
+
+    def display_text(self, colour=(255,255,255)):
+        """
+        Method for diaplying text onto the screen.
+        :attribute x: the x coordinate.
+        :attribute y: the y coordinate.
+        :attribute colour: the colour of the text.
+        """
+        font = pygame.font.Font("fonts/VT323.ttf", 50)
+        line_no = 0
+        for line in self.text:
+            imgText = font.render(line, True, colour)
+            self.screen.blit(imgText, ((self.rect.x + 5),(self.rect.y + (line_no*50))))
+            line_no += 1
+
+    def get_text(self, filepath):
+        """
+        Method which organises text from a file into a list
+        :attribute filepath: the file path to the text.
+        """ 
+
+        lines = []
+        file = open(filepath)
+        for line in file:
+            lines.append(line)
+
+        return lines
+           
---
+++ testing or planning/chapt3/textScreen/readerTEST.py
@@ -0,0 +1,38 @@
+from Writer import *
+import pygame, sys
+
+if __name__ == "__main__":
+    pygame.init()
+    screen = pygame.display.set_mode((1440,810))
+    pygame.display.set_caption("Display text test")
+    framerate = pygame.time.Clock()
+
+    background = pygame.image.load("spritesheets/background.png")
+    mouse_x = mouse_y = 0 #might do a little cheating here...
+
+    writer = Writer(screen)
+    writer.load_text("text/intro.txt")
+
+    while True:
+        framerate.tick(30)
+
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            if event.type == pygame.MOUSEMOTION:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                print(mouse_x, mouse_y)
+                #to get the coordinates of the box; a bit of a silly trick
+
+        
+        screen.blit(background, (0,0))
+        writer.display_text(colour=(223,148,243))
+        #pygame.draw.rect(screen, (0,0,255), writer.rect, width=3)
+
+        pygame.display.update()
