c8357bfeca973ac228021347805a9d6845c7c517
Anusha Din <Anusha-N-Din@hotmail.com>
2025-11-03 17:13:18 +0000

    created a satisfying bullet path for one of the Pil attacks.

--- testing or planning/chapt3/creatingAttackSeq/bullets.py
+++ testing or planning/chapt3/creatingAttackSeq/bullets.py
@@ -1,5 +1,5 @@
 from animating_sprites import *
-import pygame, math
+import pygame, math, random
 
 #this class might be placed with the devilmalz in the actual product / non-testing folders 
 class Bullets(my_sprite):
@@ -8,6 +8,7 @@ class Bullets(my_sprite):
     """
     def __init__(self, target):
         super().__init__(target)
+        self.screen = target
         self.HEIGHT = self.WIDTH = 30
         self.COLUMNS = 4
         self.load("spritesheets/pilBullets.png", self.WIDTH, self.HEIGHT, self.COLUMNS)
@@ -15,26 +16,35 @@ class Bullets(my_sprite):
         self.angle = 90
         self.rotation_angle = 0 
         self.set_x(900)
-        self.set_y(200)
+        self.set_y(random.randint(100,300)) 
+        self.dTheta = random.uniform(1,2)
 
     def update(self, current_time, rate):
         """
-        Animation update methiod for the bullet sprite.
+        Animation update method for the bullet sprite.
         :param current_time: integer time in millisecons since the sequence has started.
         :param rate: rate in milliseconds tha the sprite should update.
         """
-        MULTIPLIER = 4 #speed and size of the circle
+        if (self.get_x() > self.screen.get_width() or 
+            self.get_y() > self.screen.get_height() or
+            self.get_x() < 0 or
+            self.get_y() < 0): #proabably need to simplify this
+            self.kill()
+
+        MULTIPLIER = 8   #speed and size of the circle (not exactly)
         TOTAL_DEGREES = 360
-        self.angle = (self.angle - 1) % TOTAL_DEGREES 
+        
+        self.angle = (self.angle - self.dTheta) % TOTAL_DEGREES
+        #print(self.angle) 
         dx = math.sin(math.radians(self.angle)) * MULTIPLIER
         dy = math.cos(math.radians(self.angle)) * MULTIPLIER
 
         self.rotation_angle = (-math.degrees(math.atan2(dy,dx))) % TOTAL_DEGREES
 
-        self.x = (self.get_x() + dx)
-        self.y = (self.get_y() + dy)
+        self.set_x(self.get_x() + dx)
+        self.set_y(self.get_y() + dy)
 
-        super().update(current_time, rate, self.x, self.y)
+        super().update(current_time, rate, self.get_x(), self.get_y())
         self.image = pygame.transform.rotate(self.image, self.rotation_angle) 
 
         
