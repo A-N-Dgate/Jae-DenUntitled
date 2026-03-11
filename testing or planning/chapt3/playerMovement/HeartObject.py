38fc33f98fdbeb0e935c14bbeca727aadb653b94
Anusha Din <Anusha-N-Din@hotmail.com>
2026-03-11 19:57:31 +0000

    The player, represented by a heart, can move freely around the screen.

--- testing or planning/chapt3/playerMovement/HeartObject.py
+++ testing or planning/chapt3/playerMovement/HeartObject.py
@@ -3,32 +3,41 @@ import pygame
 class HeartObject(pygame.sprite.Sprite):
     def __init__(self, player):
         pygame.sprite.Sprite.__init__(self)
-        self.PLAYER = player
-        self.IMAGE = pygame.image.load("spritesheets/heart.png")
-        self.HITBOX = self.IMAGE.get_rect()
+        self.player = player
+        self.image = pygame.image.load("spritesheets/heart.png")
+        self.rect = self.image.get_rect()
         self.health = 50
-        self.x = 0
-        self.y = 0
+        self.rect.x = self.rect.y = 100
+        self.__MOVE_PIXELS = 3
 
     def get_player(self): return self.player
-    def get_image(self): return self.IMAGE
-    def get_hitbox(self): return self.HITBOX
+    def get_image(self): return self.image
+    def get_rect(self): return self.rect
     def get_health(self): return self.health
-    def get_x(self): return self.x
-    def get_y(self): return self.y
+    def get_x(self): return self.rect.x
+    def get_y(self): return self.rect.y
 
     def set_health(self, value): self.health = value
-    def set_x(self, value): self.x = value
-    def set_y(self, value): self.y = value
+    def set_x(self, value): self.rect.x = value
+    def set_y(self, value): self.rect.y = value
 
-    def Movement(self):
-        pass
+    def move_right(self):
+        self.set_x(self.get_x() + self.__MOVE_PIXELS)
+    
+    def move_left(self):
+        self.set_x(self.get_x() - self.__MOVE_PIXELS)
+
+    def move_up(self):
+        self.set_y(self.get_y() - self.__MOVE_PIXELS)
+
+    def move_down(self):
+        self.set_y(self.get_y() + self.__MOVE_PIXELS)
 
     def update(self):
-        pass
+        pygame.sprite.Sprite.update(self)
 
-    def draw(self): #need to check if this needs to be here?
-        pass
+    #def draw(self): #need to check if this needs to be here?
+        #pass
 
     def check_hit(self, projectile):
         pass
--- testing or planning/chapt3/playerMovement/movementTEST.py
+++ testing or planning/chapt3/playerMovement/movementTEST.py
@@ -14,21 +14,36 @@ if __name__ == "__main__":
     #sprites, objects and groups
     dummyPlayer = Character()
     heart = HeartObject(dummyPlayer)
+    heartGroup = pygame.sprite.Group()
+    heartGroup.add(heart)
     background = pygame.image.load("spritesheets/background.png")
 
     #gameplay loop
     while True:
-        framerate.tick(30)
+        framerate.tick(60)
         ticks = pygame.time.get_ticks()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+            
+            user_input = pygame.key.get_pressed()
+
+            if user_input[pygame.K_w]:
+                heart.move_up()
+            if user_input[pygame.K_s]:
+                heart.move_down()
+            if user_input[pygame.K_d]:
+                heart.move_right()
+            if user_input[pygame.K_a]:
+                heart.move_left()
+
 
         #screen display
         screen.blit(background, (0,0))
 
-        heart.update() 
+        heartGroup.update() 
+        heartGroup.draw(screen)
 
         pygame.display.update()
